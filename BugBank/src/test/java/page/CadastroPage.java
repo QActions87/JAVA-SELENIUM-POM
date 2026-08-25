86ff8bea4bac5ad82dc368e281f7f50bc2195e2c
Tiago Mota <tmota.seg@gmail.com>
2026-08-25 14:15:46 -0300

    Feat: Tornando os campos public.

--- BugBank/src/test/java/page/CadastroPage.java
+++ BugBank/src/test/java/page/CadastroPage.java
@@ -1,9 +1,9 @@
 package page;
 
 public class CadastroPage {
-    String email = "//*[@id=\"__next\"]/div/div[2]/div/div[2]/form/div[2]/input";
-    String nome = "//*[@id=\"__next\"]/div/div[2]/div/div[2]/form/div[3]/input";
-    String senha = "//*[@id=\"__next\"]/div/div[2]/div/div[2]/form/div[4]/div/input";
-    String confirmacaoSenha = "//*[@id=\"__next\"]/div/div[2]/div/div[2]/form/div[5]/div/input";
+    public String email = "//*[@id=\"__next\"]/div/div[2]/div/div[2]/form/div[2]/input";
+    public String nome = "//*[@id=\"__next\"]/div/div[2]/div/div[2]/form/div[3]/input";
+    public String senha = "//*[@id=\"__next\"]/div/div[2]/div/div[2]/form/div[4]/div/input";
+    public String confirmacaoSenha = "//*[@id=\"__next\"]/div/div[2]/div/div[2]/form/div[5]/div/input";
 
 }
