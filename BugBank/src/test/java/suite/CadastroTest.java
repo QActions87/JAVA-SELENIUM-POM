df8d4690782c84a1f2ef038059d6b95cabd7b5cb
Tiago Mota <tmota.seg@gmail.com>
2026-08-25 18:05:27 -0300

    Feat: Declaração da variável ‘cadastroPage’ que recebe a instância da classe ‘CadastroPage’.

--- BugBank/src/test/java/suite/CadastroTest.java
+++ BugBank/src/test/java/suite/CadastroTest.java
@@ -2,11 +2,44 @@ package suite;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import page.CadastroPage;
 
 public class CadastroTest {
+    // Variável para receber a instância de ChromeDriver:
     WebDriver driver;
+    // Variável para receber a instância de CadastroPage:
+    CadastroPage cadastroPage;
 
     public void before(){
+        //
         driver = new ChromeDriver();
+        // cadastroPage recebendo a instância de CadastroPage:
+        cadastroPage = new CadastroPage();
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
