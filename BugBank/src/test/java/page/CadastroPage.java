61bb8c9ca3014a1915507b269838a2ce67b14821
Tiago Mota <tmota.seg@gmail.com>
2026-08-25 15:06:31 -0300

    Feat: Declarando o WebDriver e efetuando seu import.

--- BugBank/src/test/java/page/CadastroPage.java
+++ BugBank/src/test/java/page/CadastroPage.java
@@ -1,6 +1,11 @@
 package page;
 
+import org.openqa.selenium.WebDriver;
+
 public class CadastroPage {
+    // Declarando web driver:
+    WebDriver driver;
+    // Mapeamento dos elementos da paǵina de cadastro:
     public String email = "//*[@id=\"__next\"]/div/div[2]/div/div[2]/form/div[2]/input";
     public String nome = "//*[@id=\"__next\"]/div/div[2]/div/div[2]/form/div[3]/input";
     public String senha = "//*[@id=\"__next\"]/div/div[2]/div/div[2]/form/div[4]/div/input";
