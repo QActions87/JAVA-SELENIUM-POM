cd720279cfabc2742aef30754b245524973ee407
Tiago Mota <tmota.seg@gmail.com>
2026-08-25 16:16:27 -0300

    Feat: Criando a classe de teste e declarando a variável driver do tipo WebDriver para receber a instância do ChromeDriver() dentro do método 'before'.

---
+++ BugBank/src/test/java/suite/CadastroTest.java
@@ -0,0 +1,12 @@
+package suite;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CadastroTest {
+    WebDriver driver;
+
+    public void before(){
+        driver = new ChromeDriver();
+    }
+}
