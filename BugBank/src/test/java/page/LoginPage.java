5fec4907b237a536aaf754cbbdd6dca6e4c998b2
Tiago Mota <tmota.seg@gmail.com>
2026-08-27 17:47:41 -0300

    Feat: Criando variável 'driver' para receber o driver iniciado na página de teste por meio do contrutor 'LoginPage', criado logo abaixo.

--- BugBank/src/test/java/page/LoginPage.java
+++ BugBank/src/test/java/page/LoginPage.java
@@ -1,7 +1,22 @@
 package page;
 
+import org.openqa.selenium.WebDriver;
+
 public class LoginPage {
+    // Mapeamento da página de Login:
     public String campoEmail = "//*[@id=\"__next\"]/div/div[2]/div/div[1]/form/div[1]/input";
     public String campoSenha = "//*[@id=\"__next\"]/div/div[2]/div/div[1]/form/div[2]/div/input";
     public String btnAcessar= "//*[@id=\"__next\"]/div/div[2]/div/div[1]/form/div[3]/button[1]";
+
+    // Driver para o driver da classe de teste, por meio do construtor:
+    WebDriver driver;
+
+    /** Construtor que recebe o driver dda página de testes por parâmetro.
+     * Depois, injeta este parâmetro no atributo da classe LoginPage: */
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    // Methods para interagir com os elementos mapeados:
+
 }
