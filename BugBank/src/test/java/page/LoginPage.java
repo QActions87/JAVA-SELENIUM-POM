a9430e596e098b2fb303649fd95ca70772969506
Tiago Mota <tmota.seg@gmail.com>
2026-08-27 17:27:36 -0300

    feat: Criando classe 'LoginPage' de Page Object para a página de Login, mapenado os campos E-mail e Senha, além do botão Acessar.

---
+++ BugBank/src/test/java/page/LoginPage.java
@@ -0,0 +1,7 @@
+package page;
+
+public class LoginPage {
+    public String campoEmail = "//*[@id=\"__next\"]/div/div[2]/div/div[1]/form/div[1]/input";
+    public String campoSenha = "//*[@id=\"__next\"]/div/div[2]/div/div[1]/form/div[2]/div/input";
+    public String btnAcessar= "//*[@id=\"__next\"]/div/div[2]/div/div[1]/form/div[3]/button[1]";
+}
