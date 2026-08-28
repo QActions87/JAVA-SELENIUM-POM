c84ac0e2c9bf439ce92cb00a5846378b1dc21007
Tiago Mota <tmota.seg@gmail.com>
2026-08-27 21:22:32 -0300

    Feat: Criação da variável 'loginPage' que receberá a instância da classe de Page Object 'LoginPage', que recebe como argumento o driver inicializado.

--- BugBank/src/test/java/suite/LoginTest.java
+++ BugBank/src/test/java/suite/LoginTest.java
@@ -3,13 +3,20 @@ package suite;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import page.LoginPage;
 
 public class LoginTest {
+    // Variável que receberá a instância e inicialização do ChromeDriver:
     WebDriver driver;
+    // Variável que receberá a instância da classe LoginPage, passando o driver inicializado:
+    LoginPage loginPage;
 
     @Before
     public void before() {
+        // Instanciação do driver:
         driver = new ChromeDriver();
+        // Instanciação da classe 'LoginPage' recebendo o driver inicializado como argumento:
+        loginPage = new LoginPage(driver);
     }
 }
 
