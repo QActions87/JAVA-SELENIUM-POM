8c35f8677409edd236b0f9c3e81d50094f5fdd9f
Tiago Mota <tmota.seg@gmail.com>
2026-08-28 12:34:42 -0300

    Feat: Criação da classe de Page Object da home 'HomePage' com o atributo 'driver' que receberá o driver inicializado da classe de teste por meio do construtor 'homePage()', que também foi criado aqui.

---
+++ BugBank/src/test/java/page/HomePage.java
@@ -0,0 +1,14 @@
+package page;
+
+import org.openqa.selenium.WebDriver;
+
+public class HomePage {
+    // Variável para receber o driver inicializado da classe de teste por meio do construtor:
+    WebDriver driver;
+
+    // Construtor:
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+}
