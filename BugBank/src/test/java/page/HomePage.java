7b67978dee7c45f4d2c934f89d5c812bdf446511
Tiago Mota <tmota.seg@gmail.com>
2026-08-28 12:51:59 -0300

    Feat: Mapeando o Saldo.

--- BugBank/src/test/java/page/HomePage.java
+++ BugBank/src/test/java/page/HomePage.java
@@ -6,6 +6,9 @@ public class HomePage {
     // Variável para receber o driver inicializado da classe de teste por meio do construtor:
     WebDriver driver;
 
+    // Mapeando o soldo na home:
+    public String elementoSaldo = "//*[@id=\"textBalance\"]/span";
+
     // Construtor:
     public HomePage(WebDriver driver) {
         this.driver = driver;
