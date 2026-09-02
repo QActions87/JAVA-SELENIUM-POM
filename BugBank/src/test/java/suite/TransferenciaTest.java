77d3cc799403c97390982e474f611765d34ecb17
Tiago Mota <tmota.seg@gmail.com>
2026-09-02 19:00:50 -0300

    Feat: TransferenciaTest: Além do atributo driver, instanciei as classe de PO LoginPage e CadastroPage para poder logar e criar os cadastros para o teste.

---
+++ BugBank/src/test/java/suite/TransferenciaTest.java
@@ -0,0 +1,11 @@
+package suite;
+
+import org.openqa.selenium.WebDriver;
+import page.CadastroPage;
+import page.LoginPage;
+
+public class TransferenciaTest {
+    WebDriver driver;
+    LoginPage loginPage;
+    CadastroPage cadastroPage;
+}
