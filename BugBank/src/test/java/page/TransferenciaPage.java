2a963b1a0ac923533f9da03ef2e0484f2cc4d9a6
Tiago Mota <tmota.seg@gmail.com>
2026-09-02 18:06:33 -0300

    Feat: Mapeando os elementos dos campos e botão Transferir da página Transferência.

--- BugBank/src/test/java/page/TransferenciaPage.java
+++ BugBank/src/test/java/page/TransferenciaPage.java
@@ -3,6 +3,13 @@ package page;
 import org.openqa.selenium.WebDriver;
 
 public class TransferenciaPage {
+    // Mapeia os elementos dos campos e botão da página Transferência:
+    String campoNumeroDaConta = "//*[@id=\"__next\"]/div/div[3]/form/div[1]/div[1]/input";
+    String campoDigitoConta = "//*[@id=\"__next\"]/div/div[3]/form/div[1]/div[2]/input";
+    String campoValor = "//*[@id=\"__next\"]/div/div[3]/form/div[2]/input";
+    String campoDescricao = "//*[@id=\"__next\"]/div/div[3]/form/div[3]/input";
+    String btnTransferir = "//*[@id=\"__next\"]/div/div[3]/form/button";
+
     // Variável para receber o driver inicializado da classe de teste por meio do construtor:
     WebDriver driver;
 
