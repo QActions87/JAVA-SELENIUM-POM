44e206a8b474039897f9ed606d64b76b15e187f7
Tiago Mota <tmota.seg@gmail.com>
2026-09-02 16:15:40 -0300

    feat: Criando o construtor que receberá o WebDriver inicializado do teste, que será injetado no atributo 'driver' da classe 'TransferenciaPage', que também foi criado.

--- BugBank/src/test/java/page/TransferenciaPage.java
+++ BugBank/src/test/java/page/TransferenciaPage.java
@@ -1,5 +1,13 @@
 package page;
 
-public class TransferenciaPage {
+import org.openqa.selenium.WebDriver;
 
+public class TransferenciaPage {
+    // Variável para receber o driver inicializado da classe de teste por meio do construtor:
+    WebDriver driver;
+
+    // Construtor:
+    public TransferenciaPage(WebDriver driver) {
+        this.driver = driver;
+    }
 }
