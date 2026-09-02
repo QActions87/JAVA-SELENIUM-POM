3ae69566d11544ae51acbd624c9787918c2e9b64
Tiago Mota <tmota.seg@gmail.com>
2026-09-02 18:52:12 -0300

    Feat: Criando os métodos para clicar e preencher por xpath na página de transferência.

--- BugBank/src/test/java/page/TransferenciaPage.java
+++ BugBank/src/test/java/page/TransferenciaPage.java
@@ -1,6 +1,9 @@
 package page;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class TransferenciaPage {
     // Mapeia os elementos dos campos e botão da página Transferência:
@@ -16,5 +19,20 @@ public class TransferenciaPage {
     // Construtor:
     public TransferenciaPage(WebDriver driver) {
         this.driver = driver;
+    }
+
+    // Method para preencher o campo Email:
+    public void preencherValorPorXpath(String elemento, String valor) {
+        driver.findElement(By.xpath(elemento)).sendKeys(valor);
+    }
+
+    // Method para clicar por xpath:
+    /** Clique Direto via JavaScript:
+     * Se o formulário tiver algum footer fixo ou modal invisível por cima do botão,
+     * o clique nativo do Selenium falha. Com esta solução,
+     * o JavaScript ignora a camada visual e dispara o evento direto no DOM. */
+    public void clicarPorXpath(String xpath) {
+        WebElement elemento = driver.findElement(By.xpath(xpath));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", elemento);
     }
 }
