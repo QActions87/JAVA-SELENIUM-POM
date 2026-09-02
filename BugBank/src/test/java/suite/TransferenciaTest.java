12011a2f7837686ee36acecf5b0a9f4a47e878d0
Tiago Mota <tmota.seg@gmail.com>
2026-09-02 19:05:07 -0300

    Feat: TransferenciaTest: Atributos homePage e transferenciaPage para receber suas respectivas instâncias.

--- BugBank/src/test/java/suite/TransferenciaTest.java
+++ BugBank/src/test/java/suite/TransferenciaTest.java
@@ -2,10 +2,15 @@ package suite;
 
 import org.openqa.selenium.WebDriver;
 import page.CadastroPage;
+import page.HomePage;
 import page.LoginPage;
+import page.TransferenciaPage;
 
 public class TransferenciaTest {
+    // Atributos driver e PO:
     WebDriver driver;
     LoginPage loginPage;
     CadastroPage cadastroPage;
+    HomePage homePage;
+    TransferenciaPage transferenciaPage;
 }
