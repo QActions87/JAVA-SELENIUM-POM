b652add73741a7d192c3deac7dedbacab570c96c
Tiago Mota <tmota.seg@gmail.com>
2026-09-03 12:18:14 -0300

    Feat: Finalizei a configuração do método before o WebDriver, LoginPage, CadastroPage, HomePage e TranferenciaPage por meio das variáveis de seus respectivos tipos. Depois, configurei as options para dar mais estabilidade à seção no ZorinOS Linux e passar como argumento no Driver, além de adicionar o pageLoadTimeout que define quanto tempo o Selenium deve esperar pelo carregamento completo do HTML da página ao dar um driver.get()

--- BugBank/src/test/java/suite/TransferenciaTest.java
+++ BugBank/src/test/java/suite/TransferenciaTest.java
@@ -8,8 +8,11 @@ import page.HomePage;
 import page.LoginPage;
 import page.TransferenciaPage;
 
+import java.time.Duration;
+
 public class TransferenciaTest {
-    // Atributos driver e PO:
+    // Atributos driver e variáveis que receberão as instâncias das Page Objects,
+    // para proverem os passos anteriores a transferẽncia:
     WebDriver driver;
     LoginPage loginPage;
     CadastroPage cadastroPage;
@@ -19,7 +22,16 @@ public class TransferenciaTest {
     // Preparando o ambiente:
     @Before
     public void before() {
-        driver = new ChromeDriver();
+        // Configurações para estabilizar a sessão do Chrome no Linux (Zorin OS):
+        org.openqa.selenium.chrome.ChromeOptions options = new org.openqa.selenium.chrome.ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        // Instanciação do driver passando as opções configuradas:
+        driver = new ChromeDriver(options);
+        // define quanto tempo o Selenium deve esperar pelo carregamento completo do HTML da página ao dar um driver.get():
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(5));
         loginPage = new LoginPage(driver);
         cadastroPage = new CadastroPage(driver);
         transferenciaPage = new TransferenciaPage(driver);
